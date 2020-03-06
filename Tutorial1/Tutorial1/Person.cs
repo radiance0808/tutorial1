3e8534fa9f39b74cd5e60cf7a36e0d2cc237aef1
s19230 <s19230@LAB132-10>
2020-03-06 15:35:00 +0100

    11111111111111

---
+++ Tutorial1/Tutorial1/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial1
+{
+    public class Person
+    {
+        private string firstName;
+           
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ Tutorial1/Tutorial1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tutorial1
+{
+    public class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            //    int g = 0;
+            //    bool b = true;
+
+            //    int age = 25;
+
+            //    Console.WriteLine($"I am {age} years old");
+            //    Console.WriteLine("Hello World!");
+
+            var url = @"https://pja.edu.pl/dziekanat";
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                    var matches = regex.Matches(content);
+
+                    foreach (var match in matches)
+                    {
+                        Console.WriteLine(match.ToString());
+                    }
+                }
+            }
+
+        
+            
+        }
+    }
+}
